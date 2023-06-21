1148adde3232a5d17a8cd448decbd9d3e15618df
Bishal Thapa <bishalth77@gmail.com>
2023-06-21 01:11:55 -0700

    updated

--- noteconnect/noteconnect/settings.py
+++ noteconnect/noteconnect/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'noteconnect.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'noteconnect',
+        'USER': 'postgres',
+        'PASSWORD': 'Bishal@788',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
